5f4ef7e70a2d74e95fe90a30976a78b743ac5111
Krish <mend0246@student.acitech.org>
2025-03-03 09:12:04 -0500

    WHAT I HAVE DONE WHAT NEEDS TO BE DONE WHAT WORKS WHAT DOESNT WORK

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById ("text");
+let button1 = document.getElementById("button1");
 
-button1.addEventListener("click", yell_at_user);
+let clicks = 0;
 
-function yell_at_user() {
-    alert("STOP PRESSING THIS BUTTON!")
+button1.addEventListener("click", annoy);
+
+function annoy() {
+    
+    clicks++;
+    if(clicks == 1){
+        text.innerHTML = "Hey! STOP IT";
+}
+else if(clicks == 10){
+    text.innerHTML = "I WANT YOU TO STOP DUDE";
+}
+else if(clicks == 15){
+    text.innerHTML = "I WILL HUNT YOUUUU DOWNNNNNN";
+}
+else if(clicks == 20){
+    text.innerHTML = "YOU ASKED FOR IT"
+}
+else if(clicks == 25){
+    text.innerHTML = "I'M COMIN"
+}
+else if(clicks == 30){
+    text.innerHTML = "GRRH"
+}
+else if(clicks == 35){
+    text.innerHTML = "YOUR LUCKY THAT IM 20 HOURS AWAY FROM YOU"
+}
+
 }

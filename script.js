c4b42f0e815c2e9599c69cac600850522be4a015
Krish <mend0246@student.acitech.org>
2025-03-05 09:09:26 -0500

    Made another Button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let text = document.getElementById ("text");
-let button1 = document.getElementById("button1");
+const text = document.getElementById ("text");
+const button1 = document.getElementById("button1");
+const container = document.getElementById("container");
 
 let clicks = 0;
 
@@ -20,13 +21,19 @@ else if(clicks == 15){
 }
 else if(clicks == 20){
     text.innerHTML = "YOU ASKED FOR IT"
-    button1.classList.remove("angry")
+    const button2 = document.createElement("button");
+    button2.innerHTML = "PRESS ME :)";
+    button2.classList.add("button1");
+    button2.classList.add("button2");
+    container.appendChild(button2);
 }
 else if(clicks == 25){
     text.innerHTML = "I'M COMIN"
+    button1.classList.add("button1")
 }
 else if(clicks == 30){
     text.innerHTML = "GRRH"
+    button1.classList.remove("angry")
 }
 else if(clicks == 35){
     text.innerHTML = "YOUR LUCKY THAT IM 20 HOURS AWAY FROM YOU"

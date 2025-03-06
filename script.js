e8f7cbebffc8dd65df7d8e8b808e42770faec347
Krish <mend0246@student.acitech.org>
2025-03-06 09:14:05 -0500

    Added multiple brainrot/funny sounds

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const text = document.getElementById ("text");
 const button1 = document.getElementById("button1");
 const container = document.getElementById("container");
+let eh30 = new Audio('assets/eh30.mp3')
+let creeper = new Audio('assets/creeper.mp3')
+let sidelined = new Audio('assets/sidelined.mp3')
+let MUSTARD = new Audio('assets/mustard.mp3')
+let usher = new Audio('assets/usher.mp3')
 
 let clicks = 0;
 
@@ -11,13 +16,16 @@ function annoy() {
     clicks++;
     if(clicks == 1){
         text.innerHTML = "Hey! STOP IT";
+        sidelined.play("sidelined.mp3");
 }
 else if(clicks == 10){
     text.innerHTML = "I WANT YOU TO STOP DUDE";
     button1.classList.add("angry");
+    MUSTARD.play("mustard.mp3");
 }
 else if(clicks == 15){
     text.innerHTML = "I WILL HUNT YOUUUU DOWNNNNNN";
+    usher.play("usher.mp3")
 }
 else if(clicks == 20){
     text.innerHTML = "YOU ASKED FOR IT"
@@ -26,10 +34,12 @@ else if(clicks == 20){
     button2.classList.add("button1");
     button2.classList.add("button2");
     container.appendChild(button2);
+    yay.play("eh30mp3");
 }
 else if(clicks == 25){
     text.innerHTML = "I'M COMIN"
     button1.classList.add("button1")
+    creeper.play("creeper.mp3");
 }
 else if(clicks == 30){
     text.innerHTML = "GRRH"

8ab499bfd02e0c6ac0d454f5ab653ada9f185b62
Krish <mend0246@student.acitech.org>
2025-03-07 09:14:56 -0500

    I added a sound and I add a few animations.

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const text = document.getElementById ("text");
 const button1 = document.getElementById("button1");
 const container = document.getElementById("container");
 let eh30 = new Audio('assets/eh30.mp3')
-let creeper = new Audio('assets/creeper.mp3')
 let sidelined = new Audio('assets/sidelined.mp3')
 let MUSTARD = new Audio('assets/mustard.mp3')
-let usher = new Audio('assets/usher.mp3')
+let android = new Audio('assets/android.mp3')
 
 let clicks = 0;
 
@@ -25,7 +24,7 @@ else if(clicks == 10){
 }
 else if(clicks == 15){
     text.innerHTML = "I WILL HUNT YOUUUU DOWNNNNNN";
-    usher.play("usher.mp3")
+    yay.play("eh30mp3");
 }
 else if(clicks == 20){
     text.innerHTML = "YOU ASKED FOR IT"
@@ -34,12 +33,10 @@ else if(clicks == 20){
     button2.classList.add("button1");
     button2.classList.add("button2");
     container.appendChild(button2);
-    yay.play("eh30mp3");
 }
 else if(clicks == 25){
     text.innerHTML = "I'M COMIN"
     button1.classList.add("button1")
-    creeper.play("creeper.mp3");
 }
 else if(clicks == 30){
     text.innerHTML = "GRRH"
@@ -53,5 +50,18 @@ else if(clicks == 40){
 }
 else if(clicks == 45){
     text.innerHTML = "I'll let you go"
+}
+else if(clicks == 50){
+    text.innerHTML = "SIKE"
+}
+else if(clicks == 55){
+    text.innerHTML = "Im HERE"
+}
+else if(clicks == 65){
+    text.innerHTML = "yOU jUST lOST mad AURA"
+}
+else if(clicks == 100){
+text.innerHTML = "You Win Congrats"
+android.play("android.mp3");
 }
 }
